7bc09ecab4c31b49a173fd38c1b08ae0593f568d
alfonsocamacho00089-cloud <alfonsocamacho00089@gmail.com>
2026-03-30 19:10:03 -0400

    Update P2papp.py

--- P2papp.py
+++ P2papp.py
@@ -52,11 +52,3 @@ if str(precio_alto) != str(json.load(open("p2p.json"))[0]["precio"]):
                 json.dump([resultado[0]], f, indent=4)
             print(f"¡Actualizado con éxito! precio: {precio_alto)
             enviar_notificacion_precio(precio, precio_alto)
-    else:
-            print("No se pudieron capturar las tasas.")
-
-    except Exception as e:
-        print(f"Error en la captura: {e}")
-
-if __name__ == "__main__":
-    capturar()

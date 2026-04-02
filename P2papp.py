f8c9f3a5345ee8f91fb14cc9d9df37570c52ad2a
alfonsocamacho00089-cloud <alfonsocamacho00089@gmail.com>
2026-04-02 15:10:20 -0400

    Update P2papp.py

--- P2papp.py
+++ P2papp.py
@@ -41,7 +41,6 @@ def obtener_p2p_alto():
                 # De esos 10, elegimos el más alto (el máximo)
                 return max(lista_precios)
             return "Sin anuncios"
-
 hora_actual = (datetime.datetime.now() - datetime.timedelta(hours=4)).strftime("%I:%M:%S %p")
 if "Error" in str(precio_alto) or "Sin" in str(precio_alto):
  data_p2p = [{"bank": "Binance P2P", "precio": precio_alto}]

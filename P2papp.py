dea0ff811802ced800f9bf4a4b54a5cf0d56064a
alfonsocamacho00089-cloud <alfonsocamacho00089@gmail.com>
2026-04-02 10:22:16 -0400

    Update P2papp.py

--- P2papp.py
+++ P2papp.py
@@ -14,11 +14,11 @@ def obtener_p2p_alto():
     "asset": "USDT",
     "fiat": "VES",
     "tradeType": "SELL",  # "SELL" para ver cuánto pagan por tus USDT
-    "payTypes": ["Banesco"],
+    "payTypes": ["Banesco","Pagomovil", "Mercantil"],
     "transAmount": "500",    # Un monto más común (500 Bs) para captar tasas reales
     "rows": 1,
     "page": 1,
-    "publisherType": "merchant"
+    "publisherType": "null"
     }
 
     try:
